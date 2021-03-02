d6d631a5ae6e20fe4738c848b924678990265ec7
Simon Warta <simon@warta.it>
2021-03-02 08:55:49 +0100

    Update testing contracts

--- packages/vm/src/instance.rs
+++ packages/vm/src/instance.rs
@@ -599,7 +599,7 @@ mod tests {
 
         let report2 = instance.create_gas_report();
         assert_eq!(report2.used_externally, 146);
-        assert_eq!(report2.used_internally, 51584);
+        assert_eq!(report2.used_internally, 51617);
         assert_eq!(report2.limit, LIMIT);
         assert_eq!(
             report2.remaining,
@@ -798,7 +798,7 @@ mod singlepass_tests {
             .unwrap();
 
         let init_used = orig_gas - instance.get_gas_left();
-        assert_eq!(init_used, 51730);
+        assert_eq!(init_used, 51763);
     }
 
     #[test]
@@ -821,7 +821,7 @@ mod singlepass_tests {
             .unwrap();
 
         let handle_used = gas_before_handle - instance.get_gas_left();
-        assert_eq!(handle_used, 166259);
+        assert_eq!(handle_used, 166272);
     }
 
     #[test]
